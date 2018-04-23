4948a5a0e534bf2bfb09ec99e14a129a8aaae6b0
wanglina <811197627@qq.com>
2018-04-23 09:36:03 +0800

    和

--- src/App.vue
+++ src/App.vue
@@ -17,29 +17,11 @@
 				
 		</el-menu>-->
 		<el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
-<<<<<<< HEAD
-			<el-menu-item index="1">处理中心</el-menu-item>
-			<el-submenu index="2">
-				<template slot="title">我的工作台</template>
-				<el-menu-item index="2-1">选项1</el-menu-item>
-				<el-menu-item index="2-2">选项2</el-menu-item>
-				<el-menu-item index="2-3">选项3</el-menu-item>
-				<el-submenu index="2-4">
-					<template slot="title">选项4</template>
-					<el-menu-item index="2-4-1">选项1</el-menu-item>
-					<el-menu-item index="2-4-2">选项2</el-menu-item>
-					<el-menu-item index="2-4-3">选项3</el-menu-item>
-				</el-submenu>
-			</el-submenu>
-			<el-menu-item index="3" disabled>消息中心</el-menu-item>
-			<el-menu-item index="4">订单管理</el-menu-item>
-=======
 			<el-menu-item v-if="!item.items || item.items.length===0" v-for="item in navTitle" :index="item.id">{{item.content}}</el-menu-item>
-        <el-submenu v-else :index="item.id">
-        <template slot="title">{{item.content}}</template>
-        <my-component :message="item.items"></my-component>
-      </el-submenu>
->>>>>>> 696a7c6751898182fab69a5f93eeb8d7b9fd9729
+	        <el-submenu v-else :index="item.id">
+	        <template slot="title">{{item.content}}</template>
+		        <my-component :message="item.items"></my-component>
+	        </el-submenu>
 		</el-menu>
 		 
 	</div>
@@ -48,54 +30,6 @@
 </template>
 
 <script>
-<<<<<<< HEAD
-	export default {
-		data() {
-			return {
-				activeIndex: '1',
-				navTitle: [{
-					id: 1,
-					content: "首页",
-					items: []
-				}, {
-					id: 2,
-					content: "产品服务",
-					items: [{
-						id: 1,
-						iname: "产品服务1",
-						link: ""
-					}, {
-						id: 2,
-						iname: "产品服务1",
-						link: ""
-					}]
-				}, {
-					id: 3,
-					content: "解决方案",
-					items: [{
-						id: 1,
-						iname: "解决方案1",
-						link: ""
-					}]
-				}, {
-					id: 4,
-					content: "合作伙伴",
-					items: [{
-						id: 1,
-						iname: "合作伙伴1",
-						link: ""
-					}]
-				}]
-			};
-		},
-		methods: {
-			handleSelect(key, keyPath) {
-				console.log(key, keyPath);
-			}
-		}
-	}
-=======
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from "vue";
 
 Vue.component("my-component", {
@@ -188,7 +122,6 @@ export default {
     }
   }
 };
->>>>>>> 696a7c6751898182fab69a5f93eeb8d7b9fd9729
 </script>
 
 <style>
