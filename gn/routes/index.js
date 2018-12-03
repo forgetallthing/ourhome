7d3b6dc602e146c5b618b43ba51831990c2be68d
forgetallthing <786014381@qq.com>
2018-12-03 14:16:59 +0800

    no message

--- gn/routes/index.js
+++ gn/routes/index.js
@@ -2,13 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
-
 router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/check', function(req, res, next) {
   console.log(req)
-  res.send(req.body.echostr)
+  res.send(req.echostr)
 });
 
 module.exports = router;
