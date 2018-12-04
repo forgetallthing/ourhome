c0838a21295d186c4cfa10f8b5f0e6fb084b6108
forgetallthing <786014381@qq.com>
2018-12-04 16:07:20 +0800

    定时器

--- gn/app.js
+++ gn/app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var ejs = require('ejs');
+var schedule = require("node-schedule");
+var https = require("https");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -26,12 +28,33 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
+schedule.scheduleJob('30 * * * * *', function () {
+  console.log('scheduleCronstyle:' + new Date());
+  const access_token_url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx986fbde73494c321&secret=51dd60cf87edc438b11e240cb88070d9";
+  https.get(access_token_url, function (res) {
+    var datas = [];
+    var size = 0;
+    res.on('data', function (data) {
+      datas.push(data);
+      size += data.length;
+      //process.stdout.write(data);  
+    });
+    res.on("end", function () {
+      var buff = Buffer.concat(datas, size);
+      var result = buff.toString(); 
+      console.log(result);
+    });
+  }).on("error", function (err) {
+    console.log(err)
+  });
+});
+
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
