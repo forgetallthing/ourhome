ef9223a0e9d14e9c38460a3c24518edda8e7a226
forgetallthing <786014381@qq.com>
2018-12-06 15:37:20 +0800

    2

--- gn/routes/wechatRouter.js
+++ gn/routes/wechatRouter.js
@@ -51,7 +51,7 @@ router.get('/setUserInfo', function (req, resp, next) {
                 result.access_token = access_token;
                 result.refresh_token = refresh_token;
                 wechatDao.setWechatUserInfo(result, function () {
-                  resp.send({ state: 1, value:result});
+                  resp.send({ state: 1, value: result });
                 })
               } else {
                 resp.send({ state: 0, err: result });
